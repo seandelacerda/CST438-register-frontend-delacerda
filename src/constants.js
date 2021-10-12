ff987f66efbe2595d52441dae8721963cfe04ee4
Sean DeLacerda <seanpdlc@gmail.com>
2021-10-11 18:55:11 -0700

    replace server url with local

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const SERVER_URL = 'https://cst438-registerbe-delacerda.herokuapp.com/';
+export const SERVER_URL = 'http://localhost:8080';
+// export const SERVER_URL = 'https://cst438-registerbe-delacerda.herokuapp.com/';
 
 export const SEMESTER_LIST = [
   {id:0, year:2020, name:'Fall'},
